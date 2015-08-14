ca19e168ae732a04a0340523019541763fd3efd2
Tom Parker <palfrey@tevp.net>
2015-08-14 17:45:04 +0100

    Move Solution code into separate file

---
+++ src/clj/schedule/solution.clj
@@ -0,0 +1,55 @@
+(in-ns 'schedule.types)
+
+(gen-class 
+ :name ^{PlanningSolution {}} schedule.types.ScheduleSolution
+ :extends schedule.HardSoftSolution
+ :init init
+ :state state
+ :methods [[^{PlanningEntityCollectionProperty {}} getEvents [] java.util.List]
+           [setEvents [java.util.List] void]
+           [^{ValueRangeProvider {"id" "slotRange"}} getSlotRange [] java.util.List]
+           [setFirstDay [org.joda.time.DateTime] void]
+           [setLastDay [org.joda.time.DateTime] void]
+           [getSlots [] java.util.List]
+           [setSlots [java.util.List] void]]
+ :prefix "solution-")
+
+(defn- solution-init []
+  [[] (ref {:events (ArrayList.)})])
+
+(defn- solution-getEvents [this]
+  (ArrayList. (getValue this :events)))
+
+(defn- solution-setEvents [this events]
+  (setValue this :events events))
+
+(defn- solution-getProblemFacts [this]
+  (getValue this :events))
+
+(defn- solution-getScore [this]
+  (getValue this :score))
+
+(defn- solution-setScore [this score]
+  (setValue this :score score))
+
+(defn- solution-getSlotRange [this]
+  (let [firstDay (getValue this :firstDay)
+        lastDay (getValue this :lastDay)]
+    (apply concat (for [day (p/periodic-seq firstDay (t/days 1))
+                        :while (or (t/equal? lastDay day) (t/after? lastDay day))]
+                    (map #(.getSlotsForDay % day) (getValue this :slots))))))
+
+(defn- solution-getFirstDay [this]
+  (getValue this :firstDay))
+
+(defn- solution-setFirstDay [this value]
+  (setValue this :firstDay value))
+
+(defn- solution-setLastDay [this value]
+  (setValue this :lastDay value))
+
+(defn- solution-getSlots [this]
+  (getValue this :slots))
+
+(defn- solution-setSlots [this value]
+  (setValue this :slots value))
--- src/clj/schedule/types.clj
+++ src/clj/schedule/types.clj
@@ -3,17 +3,14 @@
    [clj-time.periodic :as p]
    [clj-time.core :as t])
   (:import
-   [org.optaplanner.core.api.domain.solution PlanningSolution Solution]
-   [org.optaplanner.core.api.domain.solution PlanningEntityCollectionProperty]
    [org.optaplanner.core.api.domain.entity PlanningEntity]
    [org.optaplanner.core.api.domain.variable PlanningVariable]
-   [org.optaplanner.core.api.domain.valuerange ValueRangeProvider]
    [org.optaplanner.core.api.score.buildin.hardsoft HardSoftScore]
-
-   [schedule HardSoftSolution]
-
+   [org.optaplanner.core.api.domain.solution PlanningSolution PlanningEntityCollectionProperty]
+   [org.optaplanner.core.api.domain.valuerange ValueRangeProvider]
+   [org.joda.time ReadablePeriod DateTime Interval]
    [java.util ArrayList]
-   [org.joda.time ReadablePeriod DateTime Interval]))
+   [schedule HardSoftSolution]))
 
 (defn- getValue [this k]
   (let [state (.state this)] (.get @state k)))
@@ -21,60 +18,6 @@
 (defn- setValue [this k v]
   (let [state (.state this)]
     (dosync (alter state assoc k v)) v))
-
-(gen-class 
- :name ^{PlanningSolution {}} schedule.types.ScheduleSolution
- :extends schedule.HardSoftSolution
- :init init
- :state state
- :methods [[^{PlanningEntityCollectionProperty {}} getEvents [] java.util.List]
-           [setEvents [java.util.List] void]
-           [^{ValueRangeProvider {"id" "slotRange"}} getSlotRange [] java.util.List]
-           [setFirstDay [org.joda.time.DateTime] void]
-           [setLastDay [org.joda.time.DateTime] void]
-           [getSlots [] java.util.List]
-           [setSlots [java.util.List] void]]
- :prefix "solution-")
-
-(defn- solution-init []
-  [[] (ref {:events (ArrayList.)})])
-
-(defn- solution-getEvents [this]
-  (ArrayList. (getValue this :events)))
-
-(defn- solution-setEvents [this events]
-  (setValue this :events events))
-
-(defn- solution-getProblemFacts [this]
-  (getValue this :events))
-
-(defn- solution-getScore [this]
-  (getValue this :score))
-
-(defn- solution-setScore [this score]
-  (setValue this :score score))
-
-(defn- solution-getSlotRange [this]
-  (let [firstDay (getValue this :firstDay)
-        lastDay (getValue this :lastDay)]
-    (apply concat (for [day (p/periodic-seq firstDay (t/days 1))
-                        :while (or (t/equal? lastDay day) (t/after? lastDay day))]
-                    (map #(.getSlotsForDay % day) (getValue this :slots))))))
-
-(defn- solution-getFirstDay [this]
-  (getValue this :firstDay))
-
-(defn- solution-setFirstDay [this value]
-  (setValue this :firstDay value))
-
-(defn- solution-setLastDay [this value]
-  (setValue this :lastDay value))
-
-(defn- solution-getSlots [this]
-  (getValue this :slots))
-
-(defn- solution-setSlots [this value]
-  (setValue this :slots value))
 
 (gen-class 
  :name ^{PlanningEntity {}} schedule.types.Event
@@ -113,3 +56,5 @@
   (let [beginSlot (t/plus day (t/hours (getValue this :start)))
         endSlot (t/plus beginSlot (getValue this :length))]
     (t/interval beginSlot endSlot)))
+
+(load "solution")
