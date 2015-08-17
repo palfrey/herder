69cf17d9f83f342d179a11503248e287d3281b3f
Tom Parker <palfrey@tevp.net>
2015-08-17 17:59:20 +0100

    Forgot to properly load Slot in types...

--- src/clj/schedule/types.clj
+++ src/clj/schedule/types.clj
@@ -19,22 +19,6 @@
   (let [state (.state this)]
     (dosync (alter state assoc k v)) v))
 
-(gen-class
- :name schedule.types.Slot
- :prefix "slot-"
- :init init
- :state state
- :extends java.lang.Object
- :constructors {[java.lang.Integer org.joda.time.ReadablePeriod] []}
- :methods [[getSlotsForDay [org.joda.time.DateTime] org.joda.time.Interval]])
-
-(defn- slot-init [start length]
-  [[] (ref {:start start :length length})])
-
-(defn- slot-getSlotsForDay [this day]
-  (let [beginSlot (t/plus day (t/hours (getValue this :start)))
-        endSlot (t/plus beginSlot (getValue this :length))]
-    (t/interval beginSlot endSlot)))
-
 (load "solution")
 (load "event")
+(load "slot")
