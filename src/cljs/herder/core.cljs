98699132d1bab1529c1c2e91dd3c056493184c14
Tom Parker <palfrey@tevp.net>
2016-03-25 16:31:10 +0000

    Line up "create convention" fields in a row

--- src/cljs/herder/core.cljs
+++ src/cljs/herder/core.cljs
@@ -61,35 +61,40 @@
                        (str "Delete " name)]])]
        [:hr]
 
-       [:div {:class "form-group"}
-        [:label {:class "col-md-2 control-label text-left" :for "conventionName"} "Convention name"]
-        [:div {:class "col-md-10"}
+       [:div {:class "form-inline"}
+        [:div {:class "form-group"}
+         [:label {:for "conventionName"
+                  :style {:padding-left "5px"
+                          :padding-right "5px"}} "Convention name"]
          [:input {:id "conventionName"
                   :name "conventionName"
                   :type "text"
                   :placeholder "Convention name"
                   :class "form-control input-md"
                   :value (:name @val)
-                  :on-change #(swap! val assoc :name (-> % .-target .-value))}]]]
+                  :on-change #(swap! val assoc :name (-> % .-target .-value))}]]
 
-       [:div {:class "form-group"}
-        [:label {:class "col-md-2 control-label text-left" :for "daterange"} "Dates"]
-        [:div {:class "col-md-10"}
-         [daterange val :date]]]
-       [:button {:type "button"
-                 :class "btn btn-primary"
-                 :on-click #(do
-                              (.log js/console (pr-str @val))
-                              (POST "/api/convention"
-                                {:params
-                                 {:conventionName (:name @val)
-                                  :from (-> @val :date first js/moment (.format "YYYY-MM-DD"))
-                                  :to (-> @val :date second js/moment (.format "YYYY-MM-DD"))}
-                                 :format :json
-                                 :handler (fn [resp] (do
-                                                       (reset! val {:name "" :date nil})
-                                                       (get-convention :refresh true)))}))}
-        "Create a new convention"]])))
+        [:div {:class "form-group"}
+         [:label {:for "daterange"
+                  :style {:padding-left "5px"
+                          :padding-right "5px"}} "Dates"]
+         [daterange val :date]]
+        [:button {:type "button"
+                  :class "btn btn-primary"
+                  :style {:margin-left "5px"}
+
+                  :on-click #(do
+                               (.log js/console (pr-str @val))
+                               (POST "/api/convention"
+                                 {:params
+                                  {:conventionName (:name @val)
+                                   :from (-> @val :date first js/moment (.format "YYYY-MM-DD"))
+                                   :to (-> @val :date second js/moment (.format "YYYY-MM-DD"))}
+                                  :format :json
+                                  :handler (fn [resp] (do
+                                                        (reset! val {:name "" :date nil})
+                                                        (get-convention :refresh true)))}))}
+         "Create a new convention"]]])))
 
 (defn set-html! [dom content]
   (set! (. dom -innerHTML) content))
